513de75a098a7e9e48eca4b5f58878471786f005
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 10:44:56 +0530

    learning about

---
+++ about.java
@@ -0,0 +1,30 @@
+class about
+{
+public static void main(String[]args)
+
+{
+
+short age[]={56,45,58,44,32,41,22,18,20,66};
+System.out.println(age[0]);
+
+System.out.println(age[1]);
+
+System.out.println(age[2]);
+
+System.out.println(age[3]);
+
+System.out.println(age[4]);
+
+System.out.println(age[5]);
+
+System.out.println(age[6]);
+
+System.out.println(age[7]);
+
+System.out.println(age[8]);
+
+System.out.println(age[9]);
+
+}
+
+}
